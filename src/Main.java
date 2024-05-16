280a4e2541c15dbdf0426e1152d487932269ffd8
ferjosegonza <96452620+ferjosegonza@users.noreply.github.com>
2024-05-16 10:34:09 -0300

    añadí switch para c/ caso

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         int opcion = 0;
+        double monto = 0;
+        String monedas = null;
+        Scanner lectura = new Scanner(System.in);
         do {
-            Scanner lectura = new Scanner(System.in);
-
             System.out.println("""
                     *************************************
                     Sea bienvenido/a al Conversor de Moneda =)
@@ -25,16 +26,50 @@ public class Main {
                     """);
             try {
                 opcion = Integer.parseInt(lectura.nextLine());
+                if (opcion >= 1 && opcion <= 6){
+                    switch (opcion) {
+                        case 1:
+                            monedas = "Dólar a Peso argentino";
+                            break;
+                        case 2:
+                            monedas = "Peso argentino a Dólar";
+                            break;
+                        case 3:
+                            monedas = "Dólar a Real brasileño";
+                            break;
+                        case 4:
+                            monedas = "Real brasileño a Dólar";
+                            break;
+                        case 5:
+                            monedas = "Dólar a Peso colombiano";
+                            break;
+                        case 6:
+                            monedas = "Peso colombiano a Dólar";
+                            break;
+                        default:
+                            break;
+                    }
+
+                    do {
+                        System.out.println("Ingrese un monto para convertir de "+monedas);
+                        try {
+                            monto = Double.parseDouble(lectura.nextLine());
+                            break;
+                        } catch (NumberFormatException e){
+                            System.out.println("Valor inválido. Ingrese un número.");
+                        }
+                    } while (true);
+                    System.out.println("parece q funcó");
+                    System.out.println("monto: "+ monto);
+                    System.out.println("monedas: "+ monedas);
+                }
             } catch (NumberFormatException e){
                 System.out.println("Valor inválido. Ingrese un número entre 1 y 7.");
             }
-
         } while (opcion != 7);
 
-        if (opcion == 7){
-            System.out.println("Gracias por utilizar el conversor de moneda!");
-            System.out.println("Cerrando el programa...");
-        }
+        System.out.println("Gracias por utilizar el conversor de moneda!");
+        System.out.println("Cerrando el programa...");
 
     }
 }
